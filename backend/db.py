a31784ce57aaeb06fdbb36df72d0da1dce188e81
Marcello Iorio <marcello.iorio@outlook.com>
2026-03-04 18:55:16 -0300

    configura variaveis de ambiente com python-dotenv

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="controle_financeiro"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
